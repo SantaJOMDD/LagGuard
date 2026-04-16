9db353600383848a23adc5803656d83fa7d0f75c
SantaJOMDD <SantaJOMDD@users.noreply.github.com>
2026-04-17 00:15:24 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+const eventServer = require("./server");
+
 console.log("🚀 LagGuard backend starting...");
 
 require("./featureEngine");
 require("./predictionEngine");
+
+eventServer.start();
 
 const startNetworkCollector = require("./networkCollector");
 startNetworkCollector();
